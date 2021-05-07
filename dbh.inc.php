2667cf1ab7b143e609e57ae4453d3f7f6a24dc37
earranaga1202 <70929327+earranaga1202@users.noreply.github.com>
2021-05-06 23:12:32 -0700

    Add files via upload

---
+++ dbh.inc.php
@@ -0,0 +1,12 @@
+<?php
+
+$serverName = "mariadb";
+$dbUsername = "cs431s20";
+$dbPassword = "ohShier8";
+$dbName = "cs431s20";
+
+$conn = mysqli_connect($serverName, $dbUsername, $dbPassword, $dbName);
+
+if(!$conn){
+    die("Connection failed: " . mysqli_connect_error());
+}
