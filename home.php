a136737826ba6e7c1e881fbb27b8381cab777c98
Sagar Joshi <55200206+sagar0698@users.noreply.github.com>
2021-05-10 03:53:15 -0700

    edited, issue with listthreads

--- home.php
+++ home.php
@@ -1,71 +1,49 @@
-<?php
-    require_once("checkinfo.php");  //maybe needed, idk
-    session_start();
-    // only valid users are allowed to access the site if not they go to errorpage
-    if(!isset($_SESSION['valid_user']))
-    {
-        echo "sorry, please log in";
-        header("location: error.html");
-    }
-    $user = $_SESSION['valid_user'];
-?>
-
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
-    <head>
-        <!-- Meta Tags -->
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Main Page: 50/50</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Main Page: 50/50</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <?php include('dbconnect.php'); ?>
+    <?php include('session.php'); ?>
+</head>
+<body>
+    <?php include 'header.php'; ?>
+    <?php include 'dbconnect.php'; ?>
 
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css">
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-        <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-    </head>
-    <style>
-       body, html {
-    	height: 100%;
-            margin: 0%;
-    }
-    </style>
-    <body>
-    <header>
-        <nav class="navbar navbar-expand-lg navbar-light bg-white fixed-top">
-            <a class="navbar-brand" href="#">50/50</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNavDropdown">
-                <ul class="navbar-nav">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="logout.php" class="nav-item nav-link">Logout</a>
-                    </li>
-                </ul>
-            </div>
-        </nav>
-    </header>
+    <a href="logout.php"><button><?php echo $member_row['firstname']." ".$member_row['lastname'];?>Log Out</button></a>
+    <br> 
+    <h1 style="text-align: center;">Welcome to Fake Reddit Forum</h1>
+    <h3 style="text-align: center;">Take part of silly discussions</h3>
+	<br>
+	<br>
+    <h1 style="text-align: center;">Explore the following categories</h1>
 
-    <!-- Jumbotron with Background Image -->
-    <div class="p-5 text-center bg-image" style="
-    background-image: url('https://wallpaperaccess.com/full/750133.jpg');
-    height: 100%;
-    background-position: center;
-    background-repeat: no-repeat;
-    background-size: cover;
-    ">
-        <h1 class="mb-3">Welcome to 50/50: A Game of Chance</h1>
-        <h4 class="mb-3">Expect the Unexpected</h4>
-        <a class="btn btn-primary" href="upload.html" role="button">Create a Post</a>
-        <a class="btn btn-primary" href="upload.php" role="button">Check out Posts</a>
-    </div>
-
-
-
-
-    </body>
+<div class="card-group">
+         <?php 
+         $sql = "SELECT * FROM `categories`"; 
+         $result = mysqli_query($conn, $sql);
+         while($row = mysqli_fetch_assoc($result)){
+          $id = $row['category_id'];
+          $cat = $row['category_name'];
+          $desc = $row['category_description'];
+          echo '<div class="col-md-4 my-2 ">
+                  <div class="card">
+                      <img src="https://source.unsplash.com/500x400/?'.$id. ',food" class="card-img-top" alt="image for this category">
+                      <div class="card-body">
+                          <h5 class="card-title"><a href="threadlist.php?catid=' . $id . '">' . $cat . '</a></h5>
+                          <p class="card-text">' . substr($desc, 0, 90). '... </p>
+                          <a href="threadlist.php?catid=' . $id . '" class="btn btn-primary">View Threads</a>
+                      </div>
+                  </div>
+                </div>';
+         } 
+         ?>
+    <?php include 'footer.php';?>
+</body>
 </html>
