6aa7888513a7ed6ed687a1cfdf0882ffce034313
Sagar Joshi <55200206+sagar0698@users.noreply.github.com>
2021-05-10 03:50:59 -0700

    Create threadlist.php

---
+++ threadlist.php
@@ -0,0 +1,156 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Threadlist Forums</title>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    
+    <?php include('dbconnect.php'); ?>
+    <?php include('session.php'); ?>
+</head>
+
+<body>
+    <?php include 'header.php'; ?>
+    <?php include 'dbconnect.php'; ?>
+
+    <a href="logout.php"><button><?php echo $member_row['firstname']." ".$member_row['lastname'];?>Log Out</button></a>
+    <br> 
+
+
+    <?php
+    $id = $_GET['catid'];
+    $sql = "SELECT * FROM `categories` WHERE category_id=$id"; 
+    $result = mysqli_query($conn, $sql);
+    while($row = mysqli_fetch_assoc($result)){
+        $catname = $row['category_name'];
+        $catdesc = $row['category_description'];
+    }
+    ?>
+
+    <!-- Category container starts here -->
+    <div class="container my-4">
+        <div class="jumbotron">
+            <h1 class="display-4">Welcome to <?php echo $catname;?> forums</h1>
+            <p class="lead"> <?php echo $catdesc;?></p>
+            <hr class="my-4">
+            <p>I know this is common sense, but please be kind to one another.</p>
+            <a class="btn btn-success btn-dark" href="home.php" role="button">Choose Another Category</a>
+        </div>
+    </div>
+
+    <form method="post"> 
+					<textarea name="post_content" rows="7" cols="64" style="" placeholder="Start a discussion" required></textarea>
+					<br>
+					<button name="post">&nbsp;POST</button>
+					<br>
+					<hr>
+					</form>
+						<?php	
+							$query = mysqli_query($conn,"SELECT *,UNIX_TIMESTAMP() - date_created AS TimeSpent from post LEFT JOIN user on user.user_id = post.user_id order by post_id DESC")or die(mysqli_error());
+							while($post_row=mysqli_fetch_array($query)){
+							$id = $post_row['post_id'];	
+							$upid = $post_row['user_id'];	
+							$posted_by = $post_row['firstname']." ".$post_row['lastname'];
+						?>
+					<a style="text-decoration:none; float:right;" href="deletepost.php<?php echo '?id='.$id; ?>"><button><font color="red">x</button></font></a>
+					<h3>Posted by: <a href="#"> <?php echo $posted_by; ?></a>
+					-
+						<?php				
+								$days = floor($post_row['TimeSpent'] / (60 * 60 * 24));
+								$remainder = $post_row['TimeSpent'] % (60 * 60 * 24);
+								$hours = floor($remainder / (60 * 60));
+								$remainder = $remainder % (60 * 60);
+								$minutes = floor($remainder / 60);
+								$seconds = $remainder % 60;
+								if($days > 0)
+								echo date('F d, Y - H:i:sa', $post_row['date_created']);
+								elseif($days == 0 && $hours == 0 && $minutes == 0)
+								echo "A few seconds ago";		
+								elseif($days == 0 && $hours == 0)
+								echo $minutes.' minutes ago';
+						?>
+					<br>
+					<br><?php echo $post_row['content']; ?></h3>
+					<form method="post">
+					<hr>
+					Comment:<br>
+					<input type="hidden" name="id" value="<?php echo $id; ?>">
+					<textarea name="comment_content" rows="2" cols="44" style="" placeholder="Type your comment here........" required></textarea><br>
+					<input type="submit" name="comment">
+					</form>
+						
+					</br>
+				
+							<?php 
+								$comment_query = mysqli_query($conn,"SELECT * ,UNIX_TIMESTAMP() - date_posted AS TimeSpent FROM comment LEFT JOIN user on user.user_id = comment.user_id where post_id = '$id'") or die (mysqli_error());
+								while ($comment_row=mysqli_fetch_array($comment_query)){
+								$comment_id = $comment_row['comment_id'];
+								$comment_by = $comment_row['firstname']." ".  $comment_row['lastname'];
+							?>
+					<br><a href="#"><?php echo $comment_by; ?></a> - <?php echo $comment_row['content']; ?>
+					<br>
+							<?php				
+								$days = floor($comment_row['TimeSpent'] / (60 * 60 * 24));
+								$remainder = $comment_row['TimeSpent'] % (60 * 60 * 24);
+								$hours = floor($remainder / (60 * 60));
+								$remainder = $remainder % (60 * 60);
+								$minutes = floor($remainder / 60);
+								$seconds = $remainder % 60;
+								if($days > 0)
+								echo date('F d, Y - H:i:sa', $comment_row['date_posted']);
+								elseif($days == 0 && $hours == 0 && $minutes == 0)
+								echo "A few seconds ago";		
+								elseif($days == 0 && $hours == 0)
+								echo $minutes.' minutes ago';
+							?>
+					<br>
+							<?php
+							}
+							?>
+					<hr
+					&nbsp;
+					<?php 
+					if ($u_id = $id){
+					?>
+					
+				
+					
+					<?php }else{ ?>
+						
+					<?php
+					} } ?>
+					
+				
+							<?php
+								if (isset($_POST['post'])){
+								$post_content  = $_POST['post_content'];
+								
+								mysqli_query($conn,"insert into post (content,date_created,user_id) values ('$post_content','".strtotime(date("Y-m-d h:i:sa"))."','$user_id') ")or die(mysqli_error());
+								header('location:threadlist.php');
+								}
+							?>
+
+							<?php
+							
+								if (isset($_POST['comment'])){
+								$comment_content = $_POST['comment_content'];
+								$post_id=$_POST['id'];
+								
+								mysqli_query($conn,"insert into comment (content,date_posted,user_id,post_id) values ('$comment_content','".strtotime(date("Y-m-d h:i:sa"))."','$user_id','$post_id')") or die (mysqli_error());
+								header('location:threadlist.php');
+								}
+							?>
+
+    
+
+    
+</body>
+<?php include 'footer.php';?>
+</html>
