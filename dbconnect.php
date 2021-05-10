fac04f0693cf75033bcc9cea0839996fab8574d6
Sagar Joshi <55200206+sagar0698@users.noreply.github.com>
2021-05-10 03:58:52 -0700

    Create dbconnect.php

---
+++ dbconnect.php
@@ -0,0 +1,8 @@
+<?php
+// Connecting to the database
+$servername = "mariadb";
+$username = "cs431s41";
+$password = "EeChe9sh";
+$database = "cs431s41";
+$conn = mysqli_connect($servername, $username, $password, $database);
+?>
