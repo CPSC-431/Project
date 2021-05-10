a1607e6eb80ee6bac18d05ffaa99da65046bd66f
Sagar Joshi <55200206+sagar0698@users.noreply.github.com>
2021-05-10 03:53:51 -0700

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,9 @@
+<div class="container-fluid bg-dark text-light" style="
+position: fixed;
+left: 0;
+bottom: 0;
+width: 100%;
+text-align: center;
+";>
+Created by Sagar Joshi and Emiliano Arranaga
+</div>
