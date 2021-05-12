a4d92125e00a729a0a101c95dc4df956f21fd6df
Sagar Joshi <55200206+sagar0698@users.noreply.github.com>
2021-05-12 00:32:32 -0700

    Update threadlist.php

--- threadlist.php
+++ threadlist.php
@@ -1,36 +1,55 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" dir="ltr">
 
 <head>
+    <!-- Required meta tags -->
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Threadlist Forums</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-    
-    <?php include('dbconnect.php'); ?>
-    <?php include('session.php'); ?>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+        integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <style>
+        #ques{
+            min-height: 433px;
+        }
+    </style>
+    <title>Welcome to Bootleg Reddit</title>
 </head>
- 
+
 <body>
-    <?php include 'header.php'; ?>
+    <?php include 'parts/header.php'; ?>
     <?php include 'dbconnect.php'; ?>
-
-    <a href="logout.php"><button><?php echo $member_row['firstname']." ".$member_row['lastname'];?>Log Out</button></a>
-    <br> 
-
 
     <?php
     $id = $_GET['catid'];
     $sql = "SELECT * FROM `categories` WHERE category_id=$id"; 
     $result = mysqli_query($conn, $sql);
+ 
     while($row = mysqli_fetch_assoc($result)){
         $catname = $row['category_name'];
         $catdesc = $row['category_description'];
+    }
+    ?>
+
+    <?php
+    $showAlert = false;
+    $method = $_SERVER['REQUEST_METHOD'];  
+    if ($method == POST) {
+	//insert thread into database
+	$th_title = $_POST['title'];
+	$th_desc = $_POST['desc'];
+    	$sql = "INSERT INTO `threads` (`thread_title`, `thread_desc`, `thread_user_id`, `thread_cat_id`, `timestamp`) VALUES ('$th_title', '$th_desc', '0', '$id', '0000-00-00')"; 
+    	$result = mysqli_query($conn, $sql);
+	$showAlert = true;
+	if (showAlert) {
+		echo '<div class="alert alert-success alert-dismissible fade show" role="alert">
+  			<strong>Ayyy it worked!</strong> Your thread has been added.
+  			<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+    				<span aria-hidden="true">&times;</span>
+  			</button>
+		</div>';
+            }
     }
     ?>
 
@@ -41,116 +60,77 @@
             <p class="lead"> <?php echo $catdesc;?></p>
             <hr class="my-4">
             <p>I know this is common sense, but please be kind to one another.</p>
-            <a class="btn btn-success btn-dark" href="home.php" role="button">Choose Another Category</a>
+            <a class="btn btn-success btn-dark" href="index.php" role="button">Choose Another Category</a>
         </div>
     </div>
 
-    <form method="post"> 
-					<textarea name="post_content" rows="7" cols="64" style="" placeholder="Start a discussion" required></textarea>
-					<br>
-					<button name="post">&nbsp;POST</button>
-					<br>
-					<hr>
-					</form>
-						<?php	
-							$query = mysqli_query($conn,"SELECT *,UNIX_TIMESTAMP() - date_created AS TimeSpent from post LEFT JOIN user on user.user_id = post.user_id order by post_id DESC")or die(mysqli_error());
-							while($post_row=mysqli_fetch_array($query)){
-							$id = $post_row['post_id'];	
-							$upid = $post_row['user_id'];	
-							$posted_by = $post_row['firstname']." ".$post_row['lastname'];
-						?>
-					<a style="text-decoration:none; float:right;" href="deletepost.php<?php echo '?id='.$id; ?>"><button><font color="red">x</button></font></a>
-					<h3>Posted by: <a href="#"> <?php echo $posted_by; ?></a>
-					-
-						<?php				
-								$days = floor($post_row['TimeSpent'] / (60 * 60 * 24));
-								$remainder = $post_row['TimeSpent'] % (60 * 60 * 24);
-								$hours = floor($remainder / (60 * 60));
-								$remainder = $remainder % (60 * 60);
-								$minutes = floor($remainder / 60);
-								$seconds = $remainder % 60;
-								if($days > 0)
-								echo date('F d, Y - H:i:sa', $post_row['date_created']);
-								elseif($days == 0 && $hours == 0 && $minutes == 0)
-								echo "A few seconds ago";		
-								elseif($days == 0 && $hours == 0)
-								echo $minutes.' minutes ago';
-						?>
-					<br>
-					<br><?php echo $post_row['content']; ?></h3>
-					<form method="post">
-					<hr>
-					Comment:<br>
-					<input type="hidden" name="id" value="<?php echo $id; ?>">
-					<textarea name="comment_content" rows="2" cols="44" style="" placeholder="Type your comment here........" required></textarea><br>
-					<input type="submit" name="comment">
-					</form>
-						
-					</br>
-				
-							<?php 
-								$comment_query = mysqli_query($conn,"SELECT * ,UNIX_TIMESTAMP() - date_posted AS TimeSpent FROM comment LEFT JOIN user on user.user_id = comment.user_id where post_id = '$id'") or die (mysqli_error());
-								while ($comment_row=mysqli_fetch_array($comment_query)){
-								$comment_id = $comment_row['comment_id'];
-								$comment_by = $comment_row['firstname']." ".  $comment_row['lastname'];
-							?>
-					<br><a href="#"><?php echo $comment_by; ?></a> - <?php echo $comment_row['content']; ?>
-					<br>
-							<?php				
-								$days = floor($comment_row['TimeSpent'] / (60 * 60 * 24));
-								$remainder = $comment_row['TimeSpent'] % (60 * 60 * 24);
-								$hours = floor($remainder / (60 * 60));
-								$remainder = $remainder % (60 * 60);
-								$minutes = floor($remainder / 60);
-								$seconds = $remainder % 60;
-								if($days > 0)
-								echo date('F d, Y - H:i:sa', $comment_row['date_posted']);
-								elseif($days == 0 && $hours == 0 && $minutes == 0)
-								echo "A few seconds ago";		
-								elseif($days == 0 && $hours == 0)
-								echo $minutes.' minutes ago';
-							?>
-					<br>
-							<?php
-							}
-							?>
-					<hr
-					&nbsp;
-					<?php 
-					if ($u_id = $id){
-					?>
-					
-				
-					
-					<?php }else{ ?>
-						
-					<?php
-					} } ?>
-					
-				
-							<?php
-								if (isset($_POST['post'])){
-								$post_content  = $_POST['post_content'];
-								
-								mysqli_query($conn,"insert into post (content,date_created,user_id) values ('$post_content','".strtotime(date("Y-m-d h:i:sa"))."','$user_id') ")or die(mysqli_error());
-								header('location:threadlist.php');
-								}
-							?>
+   <div class="container">
+   <form action = "<?php echo $_SERVER['REQUEST_URI'] ?>" method = "post">
+	<h1>Start a Discussion</h1>
+  	<div class="form-group">
+    		<label for="title">Problem Title</label>
+    		<input type="text" class="form-control" id="title" name="title" aria-describedby="title">
+    		<small id="title" class="form-text text-muted">Please keep it appropriate as possible</small>
+  	</div>
+  	<div class="form-group">
+    		<label for="desc">Include your message here...</label>
+    		<textarea class="form-control" id="desc" name="desc" rows="3"></textarea>
+  	</div>  	
+	<button type="submit" class="btn btn-dark">Submit</button>
+   </form>
 
-							<?php
-							
-								if (isset($_POST['comment'])){
-								$comment_content = $_POST['comment_content'];
-								$post_id=$_POST['id'];
-								
-								mysqli_query($conn,"insert into comment (content,date_posted,user_id,post_id) values ('$comment_content','".strtotime(date("Y-m-d h:i:sa"))."','$user_id','$post_id')") or die (mysqli_error());
-								header('location:threadlist.php');
-								}
-							?>
+    </div>
+    <div class="container">
+        <h1 class="py-2">Browse Topics</h1>
+        <?php
+        $id = $_GET['catid'];
+        $sql = "SELECT * FROM `threads` WHERE thread_cat_id=$id"; 
+        $result = mysqli_query($conn, $sql);
+        $noResults = true;
+        while($row = mysqli_fetch_assoc($result)){
+            $noResults = false;
+            $id = $row['thread_id'];
+            $title = $row['thread_title'];
+            $desc = $row['thread_desc'];
 
-    
+            echo '<div class="media my-3">
+                <img src="https://techpowerusa.com/wp-content/uploads/2017/06/default-user.png" width="50px" class="mr-3" alt="default-image">
+                <div class="media-body">
+                    <h5 class="mt-0"> <a href="thread.php?threadid='. $id . '">'. $title . '</a></h5>
+                    '. $desc . '
+                </div>
+            </div>';
 
-    
+    }
+
+    // echo var_dump($noResults);  <!-- Ignore, was just used for testing purposes -->
+
+    if ($noResults) {
+	echo '<div class="jumbotron jumbotron-fluid">
+   		<div class="container">
+    			<p class="display-4">No Questions Found</p>
+    			<p class="lead">Be the first person to include a question in this forum.</p>
+  		</div>
+	     </div>';
+    }
+    ?>
+
+    </div>
+
+    <?php include 'parts/footer.php';?>
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"
+        integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous">
+    </script>
 </body>
-<?php include 'footer.php';?>
+
 </html>
