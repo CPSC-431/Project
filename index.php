ee406f01e6c5843a219c1fd03dee8e3833c8898b
Sagar Joshi <55200206+sagar0698@users.noreply.github.com>
2021-05-12 00:33:37 -0700

    Update and rename home.php to index.php

---
+++ index.php
@@ -0,0 +1,63 @@
+<!doctype html>
+<html lang="en" dir="ltr">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+        integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <style>
+        #ques{
+            min-height: 433px;
+        }
+    </style>
+    <title>Welcome to Bootleg Reddit</title>
+</head>
+
+<body>
+    <?php include 'parts/header.php'; ?>
+    <?php include 'dbconnect.php'; ?>
+
+    <br> 
+    <h1 style="text-align: center;">Welcome to Fake Reddit Forum</h1>
+    <h3 style="text-align: center;">Take part of silly discussions</h3>
+    <h1 style="text-align: center;">Explore the following categories</h1>
+
+<div class="card-group">
+         <?php 
+         $sql = "SELECT * FROM `categories`"; 
+         $result = mysqli_query($conn, $sql);
+         while($row = mysqli_fetch_assoc($result)){
+          $id = $row['category_id'];
+          $cat = $row['category_name'];
+          $desc = $row['category_description'];
+          echo '<div class="col-md-4 my-2 ">
+                  <div class="card">
+                      <img src="https://source.unsplash.com/500x400/?'.$id. ',food" class="card-img-top" alt="image for this category">
+                      <div class="card-body">
+                          <h5 class="card-title"><a href="threadlist.php?catid=' . $id . '">' . $cat . '</a></h5>
+                          <p class="card-text">' . substr($desc, 0, 90). '... </p>
+                          <a href="threadlist.php?catid=' . $id . '" class="btn btn-primary">View Threads</a>                      </div>
+                  </div>
+                </div>';
+         } 
+         ?>
+    <?php include 'parts/footer.php';?>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"
+        integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
