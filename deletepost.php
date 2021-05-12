429b5f397a588bb4577993f194a7febb7ba56288
Sagar Joshi <55200206+sagar0698@users.noreply.github.com>
2021-05-12 00:34:58 -0700

    Delete deletepost.php

--- deletepost.php
+++
@@ -1,8 +0,0 @@
-<?php 
-include ('dbconnect.php');
-$id=$_GET['id'];
-
-mysqli_query($conn,"delete from post where post_id='$id'") or die (mysqli_error());
-header ('location:threadlist.php');
-
-?>
